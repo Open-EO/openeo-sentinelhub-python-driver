61c11854501ccac4d550f5af51acdeceef9bfd4b
jjezersek <jan.jezersek@sinergise.com>
2019-07-25 10:27:53 +0100

    Initial test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,26 +2,30 @@ import json
 import pytest
 
 import sys, os
+os.environ["DYNAMODB_PRODUCTION"] = "testing"
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import app
 
+print("TEST INTEGRATION")
 
 @pytest.fixture
 def app_client():
+    print("CREATING FIXTURE!!!!!")
     app.testing = True
     return app.test_client()
 
 
 ###################################
 
-
 def test_root(app_client):
+    print("TESTING!!!!!")
     """
         Test root ('/') endpoint:
           - response must contain all the required keys
           - list of endpoints must contain at least ourselves
     """
-    r = app_client.get('/')
+    r = app_client.get('http://dynamodb:8000')
+
     assert r.status_code == 200
     actual = json.loads(r.data.decode('utf-8'))
 
@@ -41,4 +45,5 @@ def test_root(app_client):
         "path": "/",
         "methods": ["GET"],
     }
+    # print("ENDPOINTS:",actual["endpoints"])
     assert expected_endpoint in actual["endpoints"]
