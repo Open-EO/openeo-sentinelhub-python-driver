22aae1e2afaa23b7bca834b8f530be1d1c758432
jjezersek <jan.jezersek@sinergise.com>
2019-09-12 14:28:55 +0200

    Don't download again if files exist

--- download-process-definitions.sh
+++ download-process-definitions.sh
@@ -3,5 +3,5 @@ for file in $(find ./workers/process/ -type f -not -name "_*.py");
 do
 	full_filename=$(basename -- "$file")
 	filename="${full_filename%.*}"
-    wget -P ./rest/process_definitions/ https://raw.githubusercontent.com/Open-EO/openeo-processes/0.4.2/$filename.json
+    wget -P ./rest/process_definitions/ https://raw.githubusercontent.com/Open-EO/openeo-processes/0.4.2/$filename.json --no-clobber
 done
