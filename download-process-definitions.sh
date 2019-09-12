918b4342d5951890a7cc395a55929838b6c276ca
jjezersek <jan.jezersek@sinergise.com>
2019-09-12 11:35:02 +0200

    Check what happens if tests fail

--- download-process-definitions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for file in $(find ./workers/process/ -type f -not -name "_*.py");
-do
-	full_filename=$(basename -- "$file")
-	filename="${full_filename%.*}"
-    wget -P ./rest/process_definitions/ https://raw.githubusercontent.com/Open-EO/openeo-processes/0.4.2/$filename.json
-done
