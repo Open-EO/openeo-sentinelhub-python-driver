a9c2849fc23d94589a7a16492134690ac2b6c892
jjezersek <jan.jezersek@sinergise.com>
2019-10-10 13:12:12 +0200

    Get dependent nodes and set passed argument values even from more complex arguments

--- workers/process/reduce.py
+++ workers/process/reduce.py
@@ -2,8 +2,28 @@ from ._common import ProcessEOTask, ProcessArgumentInvalid, ProcessArgumentRequi
 from eolearn.core import EOWorkflow
 import process
 
+def iterate(obj):
+    if isinstance(obj, list):
+        for i,v in enumerate(obj):
+            yield i,v
+    elif isinstance(obj, dict):
+        for k, v in obj.items():
+            yield k,v
+
 class reduceEOTask(ProcessEOTask):
     def generate_workflow_dependencies(self, graph, parent_arguments):
+
+        def set_from_arguments(args, parent_arguments):
+            for key, value in iterate(args):
+                print(">>>>>>>>>>>>>>>>>>   ARGUMENT: " + key)
+                if isinstance(value, dict) and len(value) == 1 and 'from_argument' in value:
+                    args[key] = parent_arguments[value["from_argument"]]
+                    print(">>>>>>>>>>>>>>>>>> setting from argument\n")
+                    print(node_arguments)
+                    print("<<<<<<<<<<<<<<<<<<\n")
+                elif isinstance(value, dict) or isinstance(value, list):
+                    this.set_from_arguments(value, parent_arguments)
+
         result_task = None
         # input_args = {}
         tasks = {}
@@ -22,16 +42,18 @@ class reduceEOTask(ProcessEOTask):
             print(">>>>>>>>>>>>>>>>>>   NODE NAME: " + node_name)
             node_arguments = node_definition["arguments"]
 
-            for argument in node_arguments:
-                print(">>>>>>>>>>>>>>>>>>   ARGUMENT: " + argument)
-                if isinstance(node_arguments[argument],dict):
-                    print(argument)
-                    for key in node_arguments[argument]:
-                        if key == "from_argument":
-                            node_arguments[argument] = parent_arguments[node_arguments[argument][key]]
-                            print(">>>>>>>>>>>>>>>>>> setting from argument\n")
-                            print(node_arguments)
-                            print("<<<<<<<<<<<<<<<<<<\n")
+            # for argument in node_arguments:
+            #     print(">>>>>>>>>>>>>>>>>>   ARGUMENT: " + argument)
+            #     if isinstance(node_arguments[argument],dict):
+            #         print(argument)
+            #         for key in node_arguments[argument]:
+            #             if key == "from_argument":
+            #                 node_arguments[argument] = parent_arguments[node_arguments[argument][key]]
+            #                 print(">>>>>>>>>>>>>>>>>> setting from argument\n")
+            #                 print(node_arguments)
+            #                 print("<<<<<<<<<<<<<<<<<<\n")
+
+            node_arguments = self.set_from_arguments(node_arguments, parent_arguments)
 
                 # input_args[node_name] = node_arguments
 
