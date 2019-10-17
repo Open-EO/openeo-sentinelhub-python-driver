43e6456781448360cdf0a3a7a3c1065b31684f77
jjezersek <jan.jezersek@sinergise.com>
2019-10-17 12:46:15 +0200

    linear_scale_range with unit test

---
+++ workers/process/linear_scale_range.py
@@ -0,0 +1,49 @@
+import numpy as np
+import xarray as xr
+
+from ._common import ProcessEOTask, ProcessArgumentInvalid, ProcessArgumentRequired
+
+class linear_scale_rangeEOTask(ProcessEOTask):
+    """
+        This process is often used within apply process. Apply could pass each of the values separately, 
+        but this would be very inefficient. Instead, we get passed a whole xarray.
+    """
+    def process(self, arguments):
+        required_arguments = ["x", "inputMin", "inputMax"]
+
+        for required_argument in required_arguments:
+            try:
+                data = arguments[required_argument]
+            except:
+                raise ProcessArgumentRequired("Process 'linear_scale_range' requires argument '{}'.".format(required_argument))
+
+        number_arguments = ["inputMin","inputMax","outputMin","outputMax"]
+        for number_argument in number_arguments:
+            argument_value = arguments.get(number_argument)
+            if argument_value:
+                if not isinstance(argument_value, (int,float)):
+                    raise ProcessArgumentInvalid("The argument '{}' in process 'linear_scale_range' is invalid: Argument must be of type 'number'.".format(number_argument))
+
+        data, inputMin, inputMax = arguments["x"], arguments["inputMin"], arguments["inputMax"]
+        outputMin, outputMax = arguments.get("outputMin",0), arguments.get("outputMax",1)
+
+        changed_type = False
+
+        if not isinstance(data, xr.DataArray):
+            if not isinstance(data, (int,float)) and data is not None:
+                raise ProcessArgumentInvalid("The argument 'x' in process 'linear_scale_range' is invalid: Argument must be of type 'number' or 'null'.")
+
+            changed_type = True
+            data = xr.DataArray(np.array(data, dtype=np.float))
+
+            if data.size == 0:
+                return None
+
+        results = ((data - inputMin) / (inputMax - inputMin)) * (outputMax - outputMin) + outputMin
+
+        if results.size == 1 and changed_type:
+            if np.isnan(results):
+                return None
+            return float(results)
+
+        return results
