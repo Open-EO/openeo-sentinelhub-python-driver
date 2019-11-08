325e665005dab662dbc0ff6686a1d4db4fafc594
Anze Skerlavaj <anze.skerlavaj@sinergise.com>
2019-11-08 09:17:47 +0100

    Make load_collection process definition part of repo

--- download-process-definitions.sh
+++ download-process-definitions.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 for file in $(find ./workers/process/ -type f -not -name "_*.py");
 do
-	full_filename=$(basename -- "$file")
-	filename="${full_filename%.*}"
-    wget -P ./rest/process_definitions/ https://raw.githubusercontent.com/Open-EO/openeo-processes/0.4.2/$filename.json --no-clobber
+  base_filename=$(basename -- "$file")
+  filename_json="${base_filename%.*}.json"
+  if [ ! -f "./rest/process_definitions/$filename_json" ]
+  then
+    wget -P ./rest/process_definitions/ https://raw.githubusercontent.com/Open-EO/openeo-processes/0.4.2/$filename_json --no-clobber
+  fi
 done
