cdedeb4a7ae2e0cd90543f6369aacb46169f3b8a
jjezersek <jan.jezersek@sinergise.com>
2020-07-15 14:08:21 +0200

    Upgrade to v1.0: Make GET /processes valid.

--- download-process-definitions.sh
+++ download-process-definitions.sh
@@ -5,6 +5,6 @@ do
   filename_json="${base_filename%.*}.json"
   if [ ! -f "./rest/process_definitions/$filename_json" ]
   then
-    wget -P ./rest/process_definitions/ https://raw.githubusercontent.com/Open-EO/openeo-processes/0.4.2/$filename_json --no-clobber
+    wget -P ./rest/process_definitions/ https://raw.githubusercontent.com/Open-EO/openeo-processes/1.0.0-rc.1/$filename_json --no-clobber
   fi
 done
