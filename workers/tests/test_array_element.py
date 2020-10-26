0867a66d31912424ef277e7e61274bad7156d37a
jjezersek <jan.jezersek@sinergise.com>
2020-10-26 16:09:27 +0100

    Fix array_element to be v1.0.0 compatible

--- workers/tests/test_array_element.py
+++ workers/tests/test_array_element.py
@@ -1,7 +1,10 @@
 import pytest
 import sys, os
+from datetime import datetime
+
 import xarray as xr
 import numpy as np
+import pandas as pd
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import process
@@ -9,40 +12,24 @@ from process._common import ProcessParameterInvalid
 
 
 @pytest.fixture
-def generate_data():
-    def _construct(
-        data=[[[[0.1, 0.15], [0.15, 0.2]], [[0.05, 0.1], [-0.9, 0.05]]]],
-        dims=("t", "y", "x", "band"),
-        reduce_by="band",
-        as_list=False,
-    ):
-        if as_list:
-            return data
-
-        xrdata = xr.DataArray(
-            data,
-            dims=dims,
-            attrs={"reduce_by": [reduce_by]},
-        )
-        return xrdata
-
-    return _construct
-
-
-@pytest.fixture
-def execute_array_element_process(generate_data):
-    def wrapped(data_arguments={}, index=None, return_nodata=None):
-        arguments = {}
-        if data_arguments is not None:
-            arguments["data"] = generate_data(**data_arguments)
-        if index is not None:
-            arguments["index"] = index
+def execute_array_element_process():
+    def wrapped(data, index=None, label=None, return_nodata=False):
+        arguments = {"data": data}
         if return_nodata is not None:
             arguments["return_nodata"] = return_nodata
-
+        if index is not None:
+            arguments["index"] = index
+        if label is not None:
+            arguments["label"] = label
         return process.array_element.array_elementEOTask(None, "", None, {}, "arrayel1", {}).process(arguments)
 
     return wrapped
+
+
+def bands():
+    return pd.MultiIndex.from_arrays(
+        [["B04", "B08"], ["red", "nir"], [0.665, 0.842]], names=("_name", "_alias", "_wavelength")
+    )
 
 
 ###################################
@@ -54,66 +41,308 @@ def execute_array_element_process(generate_data):
     "data,return_nodata,index,expected_result",
     [([9, 8, 7, 6, 5], None, 2, 7), (["A", "B", "C"], None, 0, "A"), ([], True, 0, None)],
 )
-def test_examples(execute_array_element_process, data, index, return_nodata, expected_result):
+def test_examples(execute_array_element_process, data, return_nodata, index, expected_result):
     """
-    Test array_element process with examples from https://open-eo.github.io/openeo-api/processreference/#array_element
+    Test array_element process with examples from https://processes.openeo.org/1.0.0/#array_element
     """
-    data_arguments = {"data": data, "as_list": True}
-    result = execute_array_element_process(data_arguments=data_arguments, index=index, return_nodata=return_nodata)
+    result = execute_array_element_process(data, index=index, return_nodata=return_nodata)
     assert result == expected_result
 
 
 @pytest.mark.parametrize(
-    "data,index,reduce_by,expected_data,expected_dims",
+    "data,index,expected_result",
     [
         (
-            [[[[0.1, 0.15], [0.15, 0.2]], [[0.05, 0.1], [-0.9, 0.05]]]],
+            xr.DataArray(
+                [[[[0.1, 0.15], [0.15, 0.2]], [[0.05, 0.1], [-0.9, 0.05]]]],
+                dims=("t", "y", "x", "band"),
+                coords={
+                    "t": [
+                        datetime(2014, 3, 4),
+                    ],
+                    "band": bands(),
+                },
+                attrs={"reduce_by": ["band"]},
+            ),
             0,
-            "band",
-            [[[0.1, 0.15], [0.05, -0.9]]],
-            ("t", "y", "x"),
+            xr.DataArray(
+                [[[0.1, 0.15], [0.05, -0.9]]],
+                dims=("t", "y", "x"),
+                coords={
+                    "t": [
+                        datetime(2014, 3, 4),
+                    ],
+                },
+                attrs={"reduce_by": ["band"]},
+            ),
         ),
         (
-            [[[[0.1, 0.15], [0.15, 0.2]], [[0.05, 0.1], [-0.9, 0.05]]]],
+            xr.DataArray(
+                [[[[0.1, 0.15], [0.15, 0.2]], [[0.05, 0.1], [-0.9, 0.05]]]],
+                dims=("t", "y", "x", "band"),
+                coords={
+                    "t": [
+                        datetime(2014, 3, 4),
+                    ],
+                    "band": bands(),
+                },
+                attrs={"reduce_by": ["y"]},
+            ),
             1,
-            "y",
-            [[[0.05, 0.1], [-0.9, 0.05]]],
-            ("t", "x", "band"),
+            xr.DataArray(
+                [[[0.05, 0.1], [-0.9, 0.05]]],
+                dims=("t", "x", "band"),
+                coords={
+                    "t": [
+                        datetime(2014, 3, 4),
+                    ],
+                    "band": bands(),
+                },
+                attrs={"reduce_by": ["y"]},
+            ),
         ),
     ],
 )
-def test_with_xarray(
-    execute_array_element_process, generate_data, data, index, reduce_by, expected_data, expected_dims
-):
+def test_index(execute_array_element_process, data, index, expected_result):
     """
-    Test array_element process with xarray.DataArrays
+    Test array_element process with index parameter
     """
-    expected_result = generate_data(data=expected_data, dims=expected_dims, reduce_by=reduce_by)
-    result = execute_array_element_process(data_arguments={"data": data, "reduce_by": reduce_by}, index=index)
+    result = execute_array_element_process(data, index=index)
     xr.testing.assert_allclose(result, expected_result)
 
 
-def test_with_xarray_out_bounds(execute_array_element_process, generate_data):
+@pytest.mark.parametrize(
+    "data,label,expected_result",
+    [
+        (
+            xr.DataArray(
+                [[[[0.1, 0.15], [0.15, 0.2]], [[0.05, 0.1], [-0.9, 0.05]]]],
+                dims=("t", "y", "x", "band"),
+                coords={
+                    "t": [
+                        datetime(2014, 3, 4),
+                    ],
+                    "band": bands(),
+                },
+                attrs={"reduce_by": ["band"]},
+            ),
+            "B04",
+            xr.DataArray(
+                [[[0.1, 0.15], [0.05, -0.9]]],
+                dims=("t", "y", "x"),
+                coords={
+                    "t": [
+                        datetime(2014, 3, 4),
+                    ],
+                },
+                attrs={"reduce_by": ["band"]},
+            ),
+        ),
+        (
+            xr.DataArray(
+                [[[[0.1, 0.15], [0.15, 0.2]]], [[[0.05, 0.1], [-0.9, 0.05]]], [[[-0.05, 3.1], [0.99, 0.02]]]],
+                dims=("t", "y", "x", "band"),
+                coords={
+                    "t": [
+                        datetime(2014, 3, 4),
+                        datetime(2014, 3, 5),
+                        datetime(2014, 3, 6),
+                    ],
+                    "band": bands(),
+                },
+                attrs={"reduce_by": ["t"]},
+            ),
+            "2014-03-05",
+            xr.DataArray(
+                [[[0.05, 0.1], [-0.9, 0.05]]],
+                dims=("y", "x", "band"),
+                coords={
+                    "band": bands(),
+                },
+                attrs={"reduce_by": ["y"]},
+            ),
+        ),
+    ],
+)
+def test_label(execute_array_element_process, data, label, expected_result):
     """
-    Test array_element process with xarray.DataArrays with out of bounds index
+    Test array_element process with label parameter
     """
-    with pytest.raises(ProcessParameterInvalid) as ex:
-        result = execute_array_element_process(index=5)
-    assert ex.value.args == ("array_element", "index", "Index out of bounds.")
+    result = execute_array_element_process(data, label=label)
+    xr.testing.assert_allclose(result, expected_result)
 
 
 @pytest.mark.parametrize(
-    "data_arguments,index,expected_data,expected_dims",
+    "data,index,label,expected_error",
     [
-        ({}, 5, [[[np.nan, np.nan], [np.nan, np.nan]]], ("t", "y", "x")),
+        (
+            xr.DataArray(
+                [2],
+                dims=("t"),
+            ),
+            None,
+            None,
+            (
+                "array_element",
+                "index/label",
+                "The process 'array_element' requires either the 'index' or 'label' parameter to be set. (ArrayElementParameterMissing)",
+            ),
+        ),
+        (
+            xr.DataArray(
+                [2],
+                dims=("t"),
+            ),
+            0,
+            "B01",
+            (
+                "array_element",
+                "index/label",
+                "The process 'array_element' only allows that either the 'index' or the 'labels' parameter is set. (ArrayElementParameterConflict)",
+            ),
+        ),
+        (
+            xr.DataArray(
+                [[[[0.1, 0.15], [0.15, 0.2]]], [[[0.05, 0.1], [-0.9, 0.05]]], [[[-0.05, 3.1], [0.99, 0.02]]]],
+                dims=("t", "y", "x", "band"),
+                coords={
+                    "t": [
+                        datetime(2014, 3, 4),
+                        datetime(2014, 3, 5),
+                        datetime(2014, 3, 6),
+                    ],
+                    "band": bands(),
+                },
+                attrs={"reduce_by": ["t"]},
+            ),
+            None,
+            "2014-03-07",
+            (
+                "array_element",
+                "index/label",
+                "The array has no element with the specified index or label. (ArrayElementNotAvailable)",
+            ),
+        ),
+        (
+            xr.DataArray(
+                [[[[0.1, 0.15], [0.15, 0.2]]], [[[0.05, 0.1], [-0.9, 0.05]]], [[[-0.05, 3.1], [0.99, 0.02]]]],
+                dims=("t", "y", "x", "band"),
+                coords={
+                    "t": [
+                        datetime(2014, 3, 4),
+                        datetime(2014, 3, 5),
+                        datetime(2014, 3, 6),
+                    ],
+                    "band": bands(),
+                },
+                attrs={"reduce_by": ["t"]},
+            ),
+            15,
+            None,
+            (
+                "array_element",
+                "index/label",
+                "The array has no element with the specified index or label. (ArrayElementNotAvailable)",
+            ),
+        ),
     ],
 )
-def test_with_xarray_out_bounds_return_nodata(
-    execute_array_element_process, generate_data, data_arguments, index, expected_data, expected_dims
-):
+def test_errors(execute_array_element_process, data, label, index, expected_error):
     """
-    Test array_element process with xarray.DataArrays with out of bounds index and return_no_data
+    Test array_element errors
     """
-    expected_result = generate_data(expected_data, dims=expected_dims)
-    result = execute_array_element_process(data_arguments=data_arguments, index=index, return_nodata=True)
-    xr.testing.assert_equal(result, expected_result)
+    with pytest.raises(ProcessParameterInvalid) as ex:
+        result = execute_array_element_process(data, index=index, label=label)
+    assert ex.value.args == expected_error
+
+
+@pytest.mark.parametrize(
+    "data,index,label,expected_result",
+    [
+        (
+            xr.DataArray(
+                [[[[0.1, 0.15], [0.15, 0.2]]], [[[0.05, 0.1], [-0.9, 0.05]]], [[[-0.05, 3.1], [0.99, 0.02]]]],
+                dims=("t", "y", "x", "band"),
+                coords={
+                    "t": [
+                        datetime(2014, 3, 4),
+                        datetime(2014, 3, 5),
+                        datetime(2014, 3, 6),
+                    ],
+                    "band": bands(),
+                },
+                attrs={"reduce_by": ["t"]},
+            ),
+            4,
+            None,
+            xr.DataArray(
+                [[[np.nan, np.nan], [np.nan, np.nan]]],
+                dims=("y", "x", "band"),
+                coords={
+                    "band": bands(),
+                },
+                attrs={"reduce_by": ["t"]},
+            ),
+        ),
+        (
+            xr.DataArray(
+                [[[[0.1, 0.15], [0.15, 0.2]]], [[[0.05, 0.1], [-0.9, 0.05]]], [[[-0.05, 3.1], [0.99, 0.02]]]],
+                dims=("t", "y", "x", "band"),
+                coords={
+                    "t": [
+                        datetime(2014, 3, 4),
+                        datetime(2014, 3, 5),
+                        datetime(2014, 3, 6),
+                    ],
+                    "band": bands(),
+                },
+                attrs={"reduce_by": ["t"]},
+            ),
+            None,
+            "2014-03-07",
+            xr.DataArray(
+                [[[np.nan, np.nan], [np.nan, np.nan]]],
+                dims=("y", "x", "band"),
+                coords={
+                    "band": bands(),
+                },
+                attrs={"reduce_by": ["t"]},
+            ),
+        ),
+        (
+            xr.DataArray(
+                [[[[0.1, 0.15], [0.15, 0.2]]], [[[0.05, 0.1], [-0.9, 0.05]]], [[[-0.05, 3.1], [0.99, 0.02]]]],
+                dims=("t", "y", "x", "band"),
+                coords={
+                    "t": [
+                        datetime(2014, 3, 4),
+                        datetime(2014, 3, 5),
+                        datetime(2014, 3, 6),
+                    ],
+                    "band": bands(),
+                },
+                attrs={"reduce_by": ["band"]},
+            ),
+            None,
+            "B01",
+            xr.DataArray(
+                [[[np.nan, np.nan]], [[np.nan, np.nan]], [[np.nan, np.nan]]],
+                dims=("t", "y", "x"),
+                coords={
+                    "t": [
+                        datetime(2014, 3, 4),
+                        datetime(2014, 3, 5),
+                        datetime(2014, 3, 6),
+                    ],
+                },
+                attrs={"reduce_by": ["band"]},
+            ),
+        ),
+    ],
+)
+def test_return_nodata(execute_array_element_process, data, label, index, expected_result):
+    """
+    Test array_element with return_nodata parameter
+    """
+    result = execute_array_element_process(data, index=index, label=label, return_nodata=True)
+    xr.testing.assert_allclose(result, expected_result)
