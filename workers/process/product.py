1981db27a6882d0787ac4639eb46692cc118ff59
Anze Skerlavaj <anze.skerlavaj@sinergise.com>
2020-11-04 11:53:16 +0100

    Move old multiply process (0.4 compatible) code to product process (same in 1.0.0)

--- workers/process/product.py
+++ workers/process/product.py
@@ -1,10 +1,23 @@
-from ._common import ProcessEOTask
-from process.multiply import multiplyEOTask
+import numpy as np
+import xarray as xr
+
+from ._common import ProcessEOTask, ProcessParameterInvalid
 
 
-class productEOTask(multiplyEOTask):
-    """
-    This process is an exact alias for the multiply process. See process.multiply.multiplyEOTask.
-    """
+class productEOTask(ProcessEOTask):
+    def process(self, arguments):
+        data = self.validate_parameter(arguments, "data", required=True, allowed_types=[xr.DataArray, list])
+        ignore_nodata = self.validate_parameter(arguments, "ignore_nodata", default=True, allowed_types=[bool])
 
-    pass
+        if isinstance(data, xr.DataArray) and data.attrs.get("reduce_by"):
+            dim = data.attrs["reduce_by"]
+            return data.prod(dim=dim, skipna=ignore_nodata, keep_attrs=True)
+
+        if len(data) < 2:
+            raise ProcessParameterInvalid("product", "data", "Array must have at least 2 elements.")
+
+        original_type_was_number, data = self.convert_to_dataarray(data, as_list=True)
+
+        multiplication_array = xr.concat(data, dim="temporary_multiplication_dim")
+        results = multiplication_array.prod(dim="temporary_multiplication_dim", skipna=ignore_nodata, keep_attrs=True)
+        return self.results_in_appropriate_type(results, original_type_was_number)
