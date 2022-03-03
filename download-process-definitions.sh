23ab93bcbd473a14dd861e5780fbce1a7f7eafd8
Damjan Gostenčnik <damjan.gostencnik@sinergise.com>
2022-03-03 08:43:03 +0100

    limit downloaded processes to stable version

--- download-process-definitions.sh
+++ download-process-definitions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf ./openeo-processes
-git clone https://github.com/Open-EO/openeo-processes.git ./openeo-processes
+git clone --depth 1 --branch 1.2.0  https://github.com/Open-EO/openeo-processes.git ./openeo-processes
 cp ./openeo-processes/*.json ./rest/process_definitions/
 rm -rf ./openeo-processes
