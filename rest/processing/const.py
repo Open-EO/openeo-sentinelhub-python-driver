7180f03ba71936be899d279f4e6b5fd5390aedef
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2023-09-15 12:34:57 +0200

    remove changes for ZARR, use sh.py's MimeType.TIFF for openEO zarr and netcdf

--- rest/processing/const.py
+++ rest/processing/const.py
@@ -1,26 +1,11 @@
 from enum import Enum
-import mimetypes
 
 from sentinelhub import MimeType
 
 
-class TilingGridUnit(Enum):
-    DEGREE = "DEGREE"
-    METRE = "METRE"
-
-
-# inspired by sentinelhub.py MimeType class
-# https://github.com/sentinel-hub/sentinelhub-py/blob/master/sentinelhub/constants.py#L261
-class CustomMimeType(Enum):
-    ZARR = "zarr"
-
-    # this method is needed because mimetype.get_string() is called in construct_output in rest/processing/sentinel_hub.py
-    def get_string(self) -> str:
-        # mimetype string should be `zarr/array` for ZARR
-        # https://docs.sentinel-hub.com/api/latest/reference/#tag/batch_process/operation/createNewBatchProcessingRequest
-        if self is CustomMimeType.ZARR:
-            return "zarr/array"
-        return mimetypes.types_map["." + self.value]
+class ShBatchResponseOutput(Enum):
+    DATA = "default"
+    METADATA = "userdata"
 
 
 class SampleType(Enum):
@@ -44,33 +29,18 @@ default_sample_type_for_mimetype = {
     MimeType.PNG: SampleType.UINT8,
     MimeType.JPG: SampleType.UINT8,
     MimeType.TIFF: SampleType.FLOAT32,
-    CustomMimeType.ZARR: SampleType.FLOAT32,
 }
 
 supported_sample_types = {
     MimeType.PNG: [SampleType.UINT8, SampleType.UINT16],
     MimeType.JPG: [SampleType.UINT8],
     MimeType.TIFF: [SampleType.UINT8, SampleType.UINT16, SampleType.FLOAT32],
-    CustomMimeType.ZARR: [SampleType.UINT8, SampleType.UINT16, SampleType.FLOAT32],
 }
 
 sample_types_to_bytes = {
     SampleType.UINT8: 1,
     SampleType.UINT16: 2,
     SampleType.FLOAT32: 4,
-}
-
-# Data type/encoding. Allowed values depend on the sampleType defined in evalscript:
-# - |u1: 8-bit unsigned integer, recommended for sampleType UINT8 and AUTO,
-# - |i1: 8-bit signed integer, recommended for sampleType INT8,
-# - <u2,>u2: 16-bit unsigned integer (little and big endian, respectively), recommended for sampleType UINT16, allowed for UINT8 and AUTO,
-# - <i2,>i2: 16-bit signed integer (little and big endian, respectively), recommended for sampleType INT16, allowed for UINT8, INT8 and AUTO,
-# - <f4, >f4, <f8, >f8: float (little/big endian single precision, little/big endian double precision, respectively), recommended for sampleType FLOAT32, allowed for any sampleType.
-# Recommended values encode the chosen sampleType losslessly, while other allowed values encode the same values in a wider data type but do not add any more precision.
-sample_type_to_zarr_dtype = {
-    SampleType.UINT8: "|u1",
-    SampleType.UINT16: "<u2",
-    SampleType.FLOAT32: "<f8",
 }
 
 
@@ -88,7 +58,8 @@ class ProcessingRequestTypes(Enum):
 supported_mime_types = {
     ProcessingRequestTypes.BATCH: {
         "gtiff": MimeType.TIFF,
-        "zarr": CustomMimeType.ZARR,
+        "zarr": MimeType.TIFF,
+        "netcdf": MimeType.TIFF,
     },
     ProcessingRequestTypes.SYNC: {
         "gtiff": MimeType.TIFF,
@@ -98,6 +69,6 @@ supported_mime_types = {
 }
 
 supported_mime_types_error_msg = {
-    ProcessingRequestTypes.BATCH: "Currently only GTIFF and ZARR are supported.",
-    ProcessingRequestTypes.SYNC: "Currently supported formats are GTIFF, PNG or JPEG",
+    ProcessingRequestTypes.BATCH: "Currently supported formats are GTIFF, NETCDF and ZARR",
+    ProcessingRequestTypes.SYNC: "Currently supported formats are GTIFF, PNG and JPEG",
 }
