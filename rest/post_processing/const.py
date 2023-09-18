a176912a0358eaaa206f78f4666046abb29ef80f
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2023-09-18 15:14:50 +0200

    check if already parsed

---
+++ rest/post_processing/const.py
@@ -0,0 +1,8 @@
+from processing.const import CustomMimeType
+
+TMP_FOLDER = "/tmp/"
+
+parsed_output_file_name = {
+    CustomMimeType.ZARR: "output.zarr",
+    CustomMimeType.NETCDF: "output.nc",
+}
--- rest/post_processing/post_processing.py
+++ rest/post_processing/post_processing.py
@@ -2,62 +2,73 @@ import os
 import json
 import shutil
 
-from processing.const import ShBatchResponseOutput, ProcessingRequestTypes
+from openeoerrors import Internal
+from processing.const import CustomMimeType, ShBatchResponseOutput, ProcessingRequestTypes
 from processing.processing import new_process
 from post_processing.gtiff_parser import parse_multitemporal_gtiff_to_netcdf_zarr
+from post_processing.const import TMP_FOLDER, parsed_output_file_name
+
+
+def check_if_already_parsed(results, output_format):
+    for result in results:
+        if parsed_output_file_name[output_format] in result["Key"]:
+            return True
 
 
 def generate_subfolder_groups(batch_request_id, bucket, results):
-    data_metadata_pairs = {}
+    subfolder_groups = {}
     for result in results:
         for output in [ShBatchResponseOutput.DATA, ShBatchResponseOutput.METADATA]:
             if output.value in result["Key"]:
                 url = bucket.generate_presigned_url(object_key=result["Key"])
-                pair_key = result["Key"].replace(f"{batch_request_id}", "").replace("/", "").split(output.value)[0]
-                if pair_key not in data_metadata_pairs:
-                    data_metadata_pairs[pair_key] = {}
-                data_metadata_pairs[pair_key][output.value] = url
+                subfolder_name = (
+                    result["Key"].replace(f"{batch_request_id}", "").replace("/", "").split(output.value)[0]
+                )
+                if subfolder_name not in subfolder_groups:
+                    subfolder_groups[subfolder_name] = {}
+                subfolder_groups[subfolder_name][output.value] = url
 
-    return data_metadata_pairs
+    return subfolder_groups
 
 
-def upload_output_to_bucket(local_file_path, bucket, s3_prefix, s3_file_name):
-    print("upload output", local_file_path, bucket, s3_prefix, s3_file_name)
-
-    if os.path.isdir(local_file_path):
+def upload_output_to_bucket(local_file_path, bucket, output_format):
+    if output_format == CustomMimeType.NETCDF:
+        s3_path = local_file_path[len(f"{TMP_FOLDER}") :]
+        bucket.put_file_to_bucket(local_file_path, None, s3_path)
+    elif output_format == CustomMimeType.ZARR:
         for root, dirs, files in os.walk(local_file_path):
             for file in files:
-                print("file:", os.path.join(root, file))
-    elif os.path.isfile(local_file_path):
-        print("file:", local_file_path)
+                s3_path = os.path.join(root, file)[len(f"{TMP_FOLDER}") :]
+                bucket.put_file_to_bucket(file, None, s3_path)
+    else:
+        raise Internal(f"Unknown output format: {output_format}")
 
     # remove folder after the folder/file has been uploaded
     # shutil.rmtree( f"/tmp/{s3_prefix}")
 
 
 def parse_sh_gtiff_to_format(job, bucket):
-    results = bucket.get_data_from_bucket(prefix=job["batch_request_id"])
+    batch_request_id = job["batch_request_id"]
+    results = bucket.get_data_from_bucket(prefix=batch_request_id)
 
     process = new_process(json.loads(job["process"]), request_type=ProcessingRequestTypes.BATCH)
-    output_format = process.get_mimetype()
-    print(output_format)
+    output_format = process.get_mimetype() 
 
-    subfolder_groups = generate_subfolder_groups(job["batch_request_id"], bucket, results)
+    if check_if_already_parsed(results, output_format):
+        return
+
+    subfolder_groups = generate_subfolder_groups(batch_request_id, bucket, results)
 
     for subfolder_id, subfolder_group in subfolder_groups.items():
-        # print(subfolder_id)
         input_tiff = subfolder_group[ShBatchResponseOutput.DATA.value]
         input_metadata = subfolder_group[ShBatchResponseOutput.METADATA.value]
 
         # preventively remove directory and create it again
-        output_dir = f"/tmp/{subfolder_id}"
-        if os.path.exists(output_dir):
-            shutil.rmtree(output_dir)
-        os.mkdir(output_dir)
+        batch_output_dir = f"{batch_request_id}/{subfolder_id}"
+        tmp_dir = f"{TMP_FOLDER}{batch_output_dir}"
+        if os.path.exists(tmp_dir):
+            shutil.rmtree(tmp_dir)
+        os.makedirs(tmp_dir)
 
-        output_file_path, output_file_name = parse_multitemporal_gtiff_to_netcdf_zarr(
-            input_tiff, input_metadata, output_dir, output_format
-        )
-        print("path", output_file_path, output_file_name)
-
-        upload_output_to_bucket(output_file_path, bucket, subfolder_id, output_file_name)
+        output_file_path = parse_multitemporal_gtiff_to_netcdf_zarr(input_tiff, input_metadata, tmp_dir, output_format)
+        upload_output_to_bucket(output_file_path, bucket, output_format)
