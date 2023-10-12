b763b224f295ee649ba6baa9d57cd473d7122768
Zan Pecovnik <zan.pecovnik@sinergise.com>
2023-10-12 09:52:20 +0200

    get estimate from db for a job

--- rest/app.py
+++ rest/app.py
@@ -661,17 +661,9 @@ def estimate_job_cost(job_id):
     job = JobsPersistence.get_by_id(job_id)
     if job is None:
         raise JobNotFound()
-
-    # if estimate == 0, it has not been estimated yet, so do estimate and save it to db
-    if float(job["estimated_pu"]) == 0 and float(job["estimated_file_size"]) == 0:
-        estimated_pu, estimated_file_size = get_batch_job_estimate(
-            job["batch_request_id"], json.loads(job["process"]), job["deployment_endpoint"]
-        )
-        JobsPersistence.update_key(job_id, "estimated_pu", estimated_pu)
-        JobsPersistence.update_key(job_id, "estimated_file_size", estimated_file_size)
-    else:
-        estimated_pu = float(job["estimated_pu"])
-        estimated_file_size = float(job["estimated_file_size"])
+    
+    estimated_pu = float(job["estimated_pu"])
+    estimated_file_size = float(job["estimated_file_size"])
 
     return flask.make_response(
         jsonify(costs=estimated_pu, size=estimated_file_size),
