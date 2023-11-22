8ead0426455544fa60c73956b026a06c2663e265
grensburg <gustav.rensburg@sinergise.com>
2023-11-22 08:27:30 +0100

    make app publicly available by adding host=0.0.0.0

--- rest/app.py
+++ rest/app.py
@@ -938,4 +938,4 @@ if __name__ == "__main__":
         print("Running as HTTPS!")
         app.run(ssl_context="adhoc")
     else:
-        app.run(debug=True)
+        app.run(debug=True, host='0.0.0.0')
