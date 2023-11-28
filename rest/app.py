cbbaf02b41ef7c604fdb955f00eda6debc40495f
grensburg <gustav.rensburg@sinergise.com>
2023-11-28 14:33:08 +0100

    hardcoded well-known url

--- rest/app.py
+++ rest/app.py
@@ -920,7 +920,7 @@ def validate_process_graph():
 @with_logging
 def well_known():
     return flask.make_response(
-        jsonify(versions=[{"api_version": "1.0.0", "production": False, "url": flask.request.url_root}]), 200
+        jsonify(versions=[{"api_version": "1.0.0", "production": False, "url": "http://openeo-dev.sinergise.com/"}]), 200
     )
 
 
